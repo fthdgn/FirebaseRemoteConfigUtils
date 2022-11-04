f856876d75dd7bd21da1be05c7b34375cd3cb3d8
Fatih Doğan <fatih@socialivy.net>
2022-11-04 14:30:49 +0300

    wip

--- Sources/RemoteConfigProperty/RemoteConfigProperty.swift
+++ Sources/RemoteConfigProperty/RemoteConfigProperty.swift
@@ -1,6 +1,20 @@
-public struct RemoteConfigProperty {
-    public private(set) var text = "Hello, World!"
+import FirebaseRemoteConfig
+import FirebaseRemoteConfigSwift
 
-    public init() {
+@propertyWrapper public struct RemoteConfigProperty<T: Decodable> {
+    let key: String
+    let fallbackValue: T
+    
+    public var wrappedValue: T {
+        do {
+            let configValue = RemoteConfig.remoteConfig()[key]
+            if configValue.source == .remote || configValue.source == .default {
+                return try configValue.decoded()
+            } else {
+                return fallbackValue
+            }
+        } catch {
+            return fallbackValue
+        }
     }
 }
